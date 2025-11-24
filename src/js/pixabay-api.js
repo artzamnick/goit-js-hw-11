bc103342ec02900f416e0f5a76e457a94000db15
artem <a.m.zamriy@gmail.com>
2025-11-24 11:50:27 +0200

    api

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_KEY = '53386228-9ccf01ca4b721fbc8928b7547';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query, page = 1, per_page = 40) {
+return axios
+    .get(BASE_URL, {
+    params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page,
+        per_page,
+    },
+    })
+    .then(r => r.data);
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,53 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const GALLERY_SELECTOR = '.gallery';
+const LOADER_SELECTOR = '.loader';
+
+const galleryRef = document.querySelector(GALLERY_SELECTOR);
+const loaderRef = document.querySelector(LOADER_SELECTOR);
+const lightbox = new SimpleLightbox(`${GALLERY_SELECTOR} a`, {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+export function createGallery(images) {
+    if (!galleryRef) return;
+const markup = images
+    .map(img => {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${img.largeImageURL}">
+        <div class="photo-card">
+            <img class="photo-card__img" src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item"><b>Likes</b><br>${img.likes}</p>
+                <p class="info-item"><b>Views</b><br>${img.views}</p>
+                <p class="info-item"><b>Comments</b><br>${img.comments}</p>
+                <p class="info-item"><b>Downloads</b><br>${img.downloads}</p>
+            </div>
+            </div>
+        </a>
+        </li>
+        `;
+    })
+    .join('');
+    galleryRef.insertAdjacentHTML('beforeend', markup);
+    lightbox.refresh();
+}
+
+export function clearGallery() {
+    if (!galleryRef) return;
+    galleryRef.innerHTML = '';
+    lightbox.refresh();
+}
+
+export function showLoader() {
+    if (!loaderRef) return;
+    loaderRef.classList.add('is-active');
+}
+
+export function hideLoader() {
+    if (!loaderRef) return;
+    loaderRef.classList.remove('is-active');
+}
